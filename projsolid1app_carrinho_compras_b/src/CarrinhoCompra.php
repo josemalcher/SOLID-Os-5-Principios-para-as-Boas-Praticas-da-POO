e2f4b00be7ef05caf831eb2c896d8fc9dc8f29fe
Jose Malcher Jr <contato@josemalcher.net>
2022-12-31 16:33:45 -0300

    09 - Refactoring do Projeto - Aplicando o princípio na prática parte 2

--- projsolid1app_carrinho_compras_b/src/CarrinhoCompra.php
+++ projsolid1app_carrinho_compras_b/src/CarrinhoCompra.php
@@ -2,59 +2,27 @@
 
 namespace App;
 
+use App\Item;
+
 class CarrinhoCompra
 {
-  // atributos
+// atributos
   private $itens;
-  private $status;
-  private $valorTotal;
 
-  //métodos
   public function __construct()
   {
     $this->itens = [];
-    $this->status = 'aberto';
-    $this->valorTotal = 0;
   }
 
-  public function exibirItens()
+  public function getItens()
   {
     return $this->itens;
   }
 
-  public function adicionarItens(string $item, float $valor)
+  public function adicionarItem(Item $item)
   {
-    array_push($this->itens, ["item" => $item, "valor" => $valor]);
-
-    $this->valorTotal += $valor;
-
+    array_push($this->itens, $item);
     return true;
-  }
-
-  public function valorTotal()
-  {
-    return $this->valorTotal;
-  }
-
-  public function exibirStatus()
-  {
-    return $this->status;
-  }
-
-  public function confirmarPedido()
-  {
-    if ($this->validarCarrinho()) {
-      $this->status = 'CONFIRMADO';
-      $this->enviarEmailConcirmacao();
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  public function enviarEmailConcirmacao()
-  {
-    echo "<br>...enviando email de confirmação<br>";
   }
 
   public function validarCarrinho()
--- projsolid1app_carrinho_compras_b/src/Item.php
+++ projsolid1app_carrinho_compras_b/src/Item.php
@@ -4,5 +4,20 @@ namespace App;
 
 class Item
 {
+  private $descricao;
+  private $valor;
+
+  /**
+   * @param $descricao
+   * @param $valor
+   */
+  public function __construct()
+  {
+    $this->descricao = '';
+    $this->valor = 0;
+  }
+
+
+
 
 }
